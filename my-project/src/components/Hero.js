aa6727d031a320d1b7424a96634b14e48ac39859
DanVlf <daniel.volf01@gmail.com>
2022-11-20 19:59:44 +0100

    init

---
+++ my-project/src/components/Hero.js
@@ -0,0 +1,18 @@
+
+
+
+function Navbar() {
+    return (
+        
+            <div className="text-center text-green-500 text-6xl">
+                AHOJ
+            </div>
+        
+      
+  
+  
+    );
+  }
+  
+  export default Navbar;
+  
