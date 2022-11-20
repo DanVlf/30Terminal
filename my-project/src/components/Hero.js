28a18b0d99fa770b02796d19a54e541f61b1b921
DanVlf <daniel.volf01@gmail.com>
2022-11-20 20:02:10 +0100

    name

--- my-project/src/components/Hero.js
+++ my-project/src/components/Hero.js
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         
             <div className="text-center text-green-500 text-6xl">
-                AHOJ
+                AHOJ TEST
             </div>
         
       
