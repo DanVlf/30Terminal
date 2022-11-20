4fcce130de558aedbb30b5732ffe3a14ff41768f
DanVlf <daniel.volf01@gmail.com>
2022-11-20 20:26:09 +0100

    colors

--- my-project/src/components/Hero.js
+++ my-project/src/components/Hero.js
@@ -3,9 +3,12 @@
 
 function Navbar() {
     return (
-        
-            <div className="text-center text-green-500 text-6xl">
-                AHOJ TEST
+            <div className="h-14 bg-gradient-to-r from-cyan-500 to-blue-500">
+                <div className ="">
+                <div className="text-center text-green-500 text-6xl">
+                    AHOJ TEST
+                </div>
+                </div>
             </div>
         
       
